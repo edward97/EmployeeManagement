6ffab349e4d5a64cc5ee9af8c89c3d917c667f46
edward97 <edw.suryajaya@gmail.com>
2019-07-28 20:31:30 +0700

    Update

--- EmployeeManagement/Models/MockEmployeeRepository.cs
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -13,10 +13,17 @@ namespace EmployeeManagement.Models
         {
             _employeeList = new List<Employee>()
             {
-                new Employee() { Id = 1, Name = "Mary", Department = "HR", Email = "mary@mail.com" },
-                new Employee() { Id = 2, Name = "John", Department = "IT", Email = "john@mail.com" },
-                new Employee() { Id = 3, Name = "Sam", Department = "HR", Email = "sam@mail.com" }
+                new Employee() { Id = 1, Name = "Mary", Department = Dept.HR, Email = "mary@mail.com" },
+                new Employee() { Id = 2, Name = "John", Department = Dept.IT, Email = "john@mail.com" },
+                new Employee() { Id = 3, Name = "Sam", Department = Dept.IT, Email = "sam@mail.com" }
             };
+        }
+
+        public Employee Add(Employee employee)
+        {
+            employee.Id = _employeeList.Max(e => e.Id) + 1;
+            _employeeList.Add(employee);
+            return employee;
         }
 
         public IEnumerable<Employee> GetAllEmployee()
